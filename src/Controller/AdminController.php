de34a81a33cca6edcc4a877d05abd1366482ae0e
NirinaMao261 <nirina.rambelo@gmail.com>
2022-09-05 17:10:33 +0200

    show Dashboard action 2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,21 +2,30 @@
 
 namespace App\Controller;
 
-use PhpParser\Builder\Method;
-use Symfony\Component\Routing\Route;
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/tableau-de-bord', name: 'show-dashboard', methods ['GET'])]
+
+    #[Route('/tableau-de-bord', name:'show_dashboard', methods: ['GET'])]
     public function showDashboard(EntityManagerInterface $entityManager): Response
     {
 
         $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
         $categories = $entityManager->getRepository(Category::class)->findBy(['deletedAt' => null]);
-        
-        return $this->render('admin/show_dashboard.html.twig');
+        $users = $entityManager->getRepository(User::class)->findAll();
+
+        return $this->render('admin/show_dashboard.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories,
+            'users' => $users,
+        ]);
 
     }
 
