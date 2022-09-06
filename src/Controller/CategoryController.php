5e820140d659c40b777d96ef54da98f3b5c8887f
NirinaMao261 <nirina.rambelo@gmail.com>
2022-09-07 00:53:16 +0200

    Add Category + add Fixtures for categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,107 +2,37 @@
 
 namespace App\Controller;
 
-<<<<<<< Updated upstream
-use DateTime;
-use App\Entity\Category;
-use App\Repository\CategoryRepository;
-=======
 use App\Entity\Category;
 use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
->>>>>>> Stashed changes
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
-<<<<<<< Updated upstream
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-=======
->>>>>>> Stashed changes
 
 #[Route('/admin')]
 class CategoryController extends AbstractController
 {
-<<<<<<< Updated upstream
-    #[Route('/creer_une_categorie', name: 'create_category', methods: ['GET','POST'])]
-=======
     #[Route('/creer-une-categorie', name: 'create_category', methods: ['GET', 'POST'])]
->>>>>>> Stashed changes
     public function createCategory(Request $request, SluggerInterface $slugger, CategoryRepository $repository): Response
     {
         $category = new Category();
 
-<<<<<<< Updated upstream
-        $form = $this->createForm(CategoryFormType::class, $category)-> handleRequest($request);
-        
-=======
         $form = $this->createForm(CategoryFormType::class, $category)->handleRequest($request);
 
->>>>>>> Stashed changes
         if($form->isSubmitted() && $form->isValid()) {
 
             $category->setCreatedAt(new DateTime());
             $category->setUpdatedAt(new DateTime());
 
-<<<<<<< Updated upstream
-=======
             // L'alias nous servira à construire une URL.
             // Pour cela on utilise le $slugger :
->>>>>>> Stashed changes
             $category->setAlias($slugger->slug($category->getName()));
 
             $repository->add($category, true);
 
-<<<<<<< Updated upstream
-            $this->addFlash('success', 'La nouvelle catégorie est bien créée!');
-            return $this->redirectToRoute('show_dashboard');
-        }
-        return $this->render('admin/form/category.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-
-    } // end function update
-
-    #[Route('/archive-une-categorie/{id)', name: 'soft_delete_category', methods:['GET'])]
-    public function softDelete(Category $category, CategoryRepository $repository): Response
-    {
-            $category->setDeletedAt(new DateTime());
-
-            $repository->add($category, true);
-
-            $this->addFlash('success', 'La catégorie a bien été archové. Voir les archoves pour restaure');
-            return $this->redirectToroute('show_dashboard');
-    } // end function sofetDelete ()
-
-    #[Route('/restaure-une-categorie/{id}', name: 'restore_category', methods: ['GET'])]
-    public function restore(Category $category, CategoryRepository $repository): Response
-    {
-
-        $category->setDeleteAt(null);
-
-        $repository->add($category, true);
-
-        $this->addFlash('success', 'La catégorie a bien été restauré!');
-        return $this->redirectToRoute('show_dashboard');
-    }
-    #[Route('/supprimer-une-categorie/{id}', name: 'hard_delete_category', methods: ['GET'])]
-    public function restore(Category $category, CategoryRepository $repository): Response    
-    {
-        $category->setDeleteAt(null);
-
-        $this->addFlash('success', 'La catégorie a été definitivement supprimée du systême!');
-        return $this->redirectToRoute('show_dashboard');
-    }
-
-    }
-
-}
-
-
-=======
             $this->addFlash('success', 'La nouvelle catégorie est bien créée !');
             return $this->redirectToRoute('show_dashboard');
         }
@@ -173,4 +103,3 @@ class CategoryController extends AbstractController
     }
 
 }// end class
->>>>>>> Stashed changes
